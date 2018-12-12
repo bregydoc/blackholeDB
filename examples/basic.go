b3992cfcfe82d78a095fbd6745f88eeb3e3370cd
Nathan Toups <n@rjrbt.io>
2018-12-12 06:54:06 -0600

    updated PrivateKey/EncryptKey pattern with more secure defaults

--- examples/basic.go
+++ examples/basic.go
@@ -1,26 +1,36 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
+	"log"
+
 	"github.com/bregydoc/blackholeDB"
 )
 
 func main() {
-	db, err := blackhole.Open(blackhole.DefaultOptions)
+
+	// initialize db options
+	opts := blackhole.DefaultOptions
+
+	// Set PrivateKey. This should come from an ENV or a secret store in the real world
+	opts.PrivateKey, _ = hex.DecodeString("44667768254d593b7ea48c3327c18a651f6031554ca4f5e3e641f6ff1ea72e98")
+
+	db, err := blackhole.Open(opts)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	defer db.Close()
 
 	err = db.Set("answer", []byte("42"))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	answer, err := db.Get("answer")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	fmt.Println("The answer of the life is: ", string(answer))
